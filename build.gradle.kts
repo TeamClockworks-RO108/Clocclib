ce0696a57c1f4e56a12fffb278d28f23db760ae7
lucaci32u4 <lucaci32u4@gmail.com>
2025-05-01 18:19:42 +0300

    Fix annotations and increment version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-val libVersion by extra { "0.3.5" }
+val libVersion by extra { "0.3.7" }
 val mavenUsername by extra { System.getenv("MAVEN_USERNAME") ?: project.findProperty("mavenUsername") }
 val mavenPassword by extra { System.getenv("MAVEN_PASSWORD") ?: project.findProperty("mavenPassword") }
 
